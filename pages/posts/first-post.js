1f0e75c1e913fdab93d04dbb8b9bb4e8dee0ea0c
jeremygradisher <jeremygradisher@gmail.com>
2023-03-01 23:36:29 -0500

    Adding styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,17 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
-      <p>
-      The Link component enables client-side navigation between two pages in the same Next.js app.<br /><br />
-
-      Client-side navigation means that the page transition happens using JavaScript, which is faster than 
-      the default navigation done by the browser.<br /><br />
-
-      Here’s a simple way you can verify it:<br /><br />
-
-      Use the browser’s developer tools to change the background CSS property of html to yellow.<br /><br />
-      Click on the links to go back and forth between the two pages.<br /><br />
-      You’ll see that the yellow background persists between page transitions.<br /><br />
-      This shows that the browser does not load the full page and client-side navigation is working.
-      </p>
-    </>
+    </Layout>
   );
 }
